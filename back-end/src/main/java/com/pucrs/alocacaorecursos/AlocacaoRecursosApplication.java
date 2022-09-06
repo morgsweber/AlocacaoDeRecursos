0dfc14f3474a605606efedfd0147f472b252d53c
ALEXEI MILITZ ABIANNA <alexei.abianna@edu.pucrs.br>
2022-09-06 18:09:00 -0300

    start projeto

---
+++ back-end/src/main/java/com/pucrs/alocacaorecursos/AlocacaoRecursosApplication.java
@@ -0,0 +1,13 @@
+package com.pucrs.alocacaorecursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocacaoRecursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlocacaoRecursosApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/pucrs/alocacaorecursos/AlocacaoRecursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.alocacaorecursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocacaoRecursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
