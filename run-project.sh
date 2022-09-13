2519b7ea588f8b02d6fea35ae86e1095b44f7c60
Alexei Abianna <48830898+Alexeiabianna@users.noreply.github.com>
2022-09-13 18:39:30 -0300

    ajuste

--- run-project.sh
+++ run-project.sh
@@ -1,2 +1,2 @@
 cd back-end/alocacao-recursos-application/target 
-java -jar ./alocacao-recursos-application-0.0.1-SNAPSHOT.jar.original --spring.profiles.active=local
+java -jar ./alocacao-recursos-application-0.0.1-SNAPSHOT.jar --spring.profiles.active=local
