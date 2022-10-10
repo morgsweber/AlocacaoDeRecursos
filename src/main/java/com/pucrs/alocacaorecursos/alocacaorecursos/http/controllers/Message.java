d8e75a16c65be2a3097bb04f6f5c0f52c709029e
Alexeiabianna <alexeiabianna@gmail.com>
2022-10-09 21:57:29 -0300

    nova estrutura

---
+++ src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/http/controllers/Message.java
@@ -0,0 +1,13 @@
+package com.pucrs.alocacaorecursos.alocacaorecursos.http.controllers;
+
+public class Message {
+    private final String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
