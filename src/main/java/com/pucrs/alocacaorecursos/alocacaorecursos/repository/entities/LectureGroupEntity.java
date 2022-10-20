29294ad4ba9c9e97b28345c55a0c394bcbe302d5
ALEXEI MILITZ ABIANNA <ALEXEI.ABIANNA@edu.pucrs.br>
2022-10-20 18:55:13 -0300

    add table

---
+++ src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/repository/entities/LectureGroupEntity.java
@@ -0,0 +1,16 @@
+package com.pucrs.alocacaorecursos.alocacaorecursos.repository.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "turma" )
+public class LectureGroupEntity {
+
+    @Id
+    private String id;
+}
