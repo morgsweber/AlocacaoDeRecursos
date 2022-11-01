273a1e1c6c6d32ad0e473ed4bcbd6c630f182d84
ALEXEI MILITZ ABIANNA <alexei.abianna@edu.pucrs.br>
2022-11-01 19:02:19 -0300

    add name teacher

--- src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/business/teacher/TeacherLecturesViewImpl.java
+++ src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/business/teacher/TeacherLecturesViewImpl.java
@@ -10,8 +10,10 @@ import org.springframework.stereotype.Component;
 
 import com.pucrs.alocacaorecursos.alocacaorecursos.core.input.TeacherLecturesView;
 import com.pucrs.alocacaorecursos.alocacaorecursos.core.output.LecturePortOutput;
+import com.pucrs.alocacaorecursos.alocacaorecursos.core.output.TeacherPortOutput;
 import com.pucrs.alocacaorecursos.alocacaorecursos.core.output.TeachesPortOutput;
 import com.pucrs.alocacaorecursos.alocacaorecursos.domain.Lecture;
+import com.pucrs.alocacaorecursos.alocacaorecursos.domain.Teacher;
 import com.pucrs.alocacaorecursos.alocacaorecursos.domain.Teaches;
 import com.pucrs.alocacaorecursos.alocacaorecursos.domain.dto.teacher.TeacherLecturesResponse;
 
@@ -23,20 +25,25 @@ public class TeacherLecturesViewImpl implements TeacherLecturesView {
 
     @Autowired
     private LecturePortOutput lecturePortOutput;
+
+    @Autowired
+    private TeacherPortOutput teacherPortOutput;
     
     @Override
     public List<TeacherLecturesResponse> getTeacherLectures(String teacherId) {
 
         //TODO adicionar comportamento de percorrer itens para montar responses
         final List<Teaches> teaches = teachesPortOutput.getTeaches(teacherId);
+
         final Integer lectureId = teaches.get(0).getDisciplinaId(); //Ver uma forma de pegar os id da lista retornada para realizar a busca na tabela disciplinas
         final Lecture lecture = lecturePortOutput.getLecture(lectureId);
+        final Teacher teacher = teacherPortOutput.getTeacher(teacherId);
 
         System.out.println("Id:" + lecture.getId());
 
         final List<TeacherLecturesResponse> response = new ArrayList();
 
-        teaches.forEach(item -> response.add(new TeacherLecturesResponse(lecture.getName(),null,null)));
+        teaches.forEach(item -> response.add(new TeacherLecturesResponse(lecture.getName(),teacher.getName(),null)));
         
         return response;
     }
---
+++ src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/repository/adapter/mapper/TeacherMapper.java
@@ -0,0 +1,24 @@
+package com.pucrs.alocacaorecursos.alocacaorecursos.repository.adapter.mapper;
+
+import java.util.Objects;
+
+import com.pucrs.alocacaorecursos.alocacaorecursos.domain.Teacher;
+import com.pucrs.alocacaorecursos.alocacaorecursos.repository.entities.TeacherEntity;
+
+public class TeacherMapper {
+    
+    public static Teacher mapFrom(final TeacherEntity entity) {
+
+        Teacher domain = new Teacher();
+
+        if(Objects.nonNull(entity)) {
+            domain.setId(getNonNull(entity.getId()));
+            domain.setName(getNonNull(entity.getName()));
+        }
+        return domain;
+    }
+
+    private static String getNonNull(final String item) {
+        return Objects.nonNull(item) ? item : null;
+    }
+}
