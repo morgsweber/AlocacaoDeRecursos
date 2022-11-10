960a2e251db2f8740de54358b354e64c8ab4222a
ALEXEI MILITZ ABIANNA <alexei.abianna@edu.pucrs.br>
2022-11-10 18:35:19 -0300

    add cors

--- src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/http/controllers/TeacherController.java
+++ src/main/java/com/pucrs/alocacaorecursos/alocacaorecursos/http/controllers/TeacherController.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.pucrs.alocacaorecursos.alocacaorecursos.domain.dto.teacher.TeacherLec
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/*", maxAge = 3600)
 @RequestMapping("/professor")
 public class TeacherController {
 
